1b5e006131a10509be5cfc05d144338887ac0a5e
DinuGabrielaLoredana <gdinu93@yahoo.com>
2018-07-01 12:49:37 +0300

    Update ConnectionMySQL.java

--- ConnectionMySQL.java
+++ ConnectionMySQL.java
@@ -1,6 +1,6 @@
 package external_lib;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
+//import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
